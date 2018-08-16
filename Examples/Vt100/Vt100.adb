eb78311e96575e35d7466bad954c95d1365473f8
Alex Gamper <agamper@bigpond.net.au>
2018-08-16 19:39:34 +1000

    Add Vt100 example

---
+++ Examples/Vt100/Vt100.adb
@@ -0,0 +1,28 @@
+﻿--------------------------------------------------------------------------------
+with Win32;                             use Win32;
+with Interfaces.C;                      use Interfaces.C;
+With Ada.Text_IO;                       use Ada.Text_IO;
+--------------------------------------------------------------------------------
+procedure Vt100 is
+
+    Stdin       : HANDLE := GetStdHandle(STD_INPUT_HANDLE);
+    Stdout      : HANDLE := GetStdHandle(STD_OUTPUT_HANDLE);
+    StdInMode   : aliased DWORD;
+    StdOutMode  : aliased DWORD;
+    Result      : BOOL;
+    ResultCode  : DWORD;
+
+begin
+
+    Result := GetConsoleMode(StdIn , StdInMode'unchecked_access);
+    StdInMode := StdInMode or ENABLE_VIRTUAL_TERMINAL_INPUT;
+
+    Result := GetConsoleMode(StdOut , StdOutMode'unchecked_access);
+    StdOutMode := StdOutMode or ENABLE_VIRTUAL_TERMINAL_PROCESSING or DISABLE_NEWLINE_AUTO_RETURN;
+
+    Result := SetConsoleMode (StdIn, StdInMode);
+    Result := SetConsoleMode (StdOut, StdOutMode);
+
+    Put_Line(Ascii.ESC & "[93m" & "Howdy!" & Ascii.ESC & "[0m"); 
+
+end;
